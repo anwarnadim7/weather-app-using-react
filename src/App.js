15ef533a017822d5673e780c1e205355b4e5dcce
anwarnadim7 <anwarnadim3@gmail.com>
2023-04-19 02:21:06 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+       Developed with ❤️ by{" "}
+        <a target="_blank" href="">
+          Nadim Anwar
+        </a>{" "}
+     
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "d44b83e8a9ee934153d6bb594d0fe135",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
